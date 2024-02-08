81270767ccf17e93ff259a2c94be7bd37bb4d913
santhosh116 <santhosh1161999@gmail.com>
2024-02-08 23:05:22 +0530

    BuySellStock problem
    Solution for BuySellStock problem

---
+++ src/kotlin/com/santhosh/dsa/BuySellStock.kt
@@ -0,0 +1,32 @@
+package com.santhosh.dsa
+
+import java.util.Scanner;
+
+class BuySellStock {
+    fun solve(arr: IntArray, n: Int) : Int {
+        var minValue = arr[0]
+        var maxProfit = 0
+        for(i in 0 until n) {
+            if(arr[i] - minValue > maxProfit) {
+                maxProfit = arr[i] - minValue;
+            } else if(arr[i] < minValue) {
+                minValue = arr[i]
+            }
+        }
+        return maxProfit;
+    }
+
+}
+
+fun main() {
+    val sc = Scanner(System.`in`)
+    println("Enter the number of days")
+    var n = sc.nextInt();
+    println("Enter the stock price for each day")
+    var arr = IntArray(n)
+    for(i in 0 until n) {
+        arr[i] = sc.nextInt()
+    }
+    var obj = BuySellStock();
+    println("Solution ${obj.solve(arr, n)}")
+}
